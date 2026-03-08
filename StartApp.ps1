9fcf6d925b932d1fc9fe414642ffa6b79071f192
ngo.dananh1931@gmail.com <ngo.dananh1931@gmail.com>
2026-03-07 22:56:43 -0500

    Add install commands to PS script

--- StartApp.ps1
+++ StartApp.ps1
@@ -1,10 +1,12 @@
 Set-Location -Path $PSScriptRoot
 
 Push-Location "$PSScriptRoot\Frontend"
+npm install
 npm run build
 Pop-Location
 
 Push-Location "$PSScriptRoot\Backend"
+npm install
 npm run dev
 Pop-Location
 
